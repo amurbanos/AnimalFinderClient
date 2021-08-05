e4e0e48daebeabfb7394d38cc37648d1a90ff67f
¨Abraao <eumesmou>
2021-08-05 09:52:31 -0300

    Todas indentacoes convertidas pra espacos

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const global = {
   // apiUrl: 'http://172.17.0.2:3000'
-  apiUrl: 'https://api-animalfinder.urbanodata.com.br/api/v1/pets?page=1'
+  apiUrl: 'https://api-animalfinder.urbanodata.com.br'
 }
